a1e2abb6e47af41e32a77069c9db7da6e56745d9
maxlune <85277579+maxlune@users.noreply.github.com>
2024-05-15 16:21:11 +0200

    added test for MovieService

--- packages/server/src/infrastructure/data/schema/movies.ts
+++ packages/server/src/infrastructure/data/schema/movies.ts
@@ -1,5 +1,4 @@
 import { integer, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
-import { users } from "./users";
 
 export const movies = pgTable("movies", {
   id: uuid("id").defaultRandom().primaryKey(),
---
+++ packages/server/src/tests/unit/MovieService.spec.ts
@@ -0,0 +1,46 @@
+import { beforeAll, describe, it, expect } from "@jest/globals";
+import { sql } from "drizzle-orm";
+import { createdUser } from "../jest.setup";
+import { db } from "../../infrastructure/data";
+import { MovieService } from "../../domain/services/MovieService";
+import { NewMovie } from "../../domain/entities/Movie";
+
+function expectNullableAny(value: any) {
+  if (value !== null) {
+    expect(value).toEqual(expect.anything());
+  } else {
+    expect(value).toBeNull();
+  }
+}
+
+describe("MovieService", () => {
+  let movieService: MovieService;
+  let createdMovieID: string | undefined;
+  let newMovie: NewMovie = {
+    title: "Interstellar",
+    year: 2014,
+  };
+
+  beforeAll(async () => {
+    movieService = new MovieService();
+    newMovie.id = createdUser.id;
+    await db.execute(sql`SET search_path TO test`);
+  });
+
+  it("should get all movies", async () => {
+    const movies = await movieService.getAllMovies();
+    movies.forEach((movie: any) => {
+      expect(movie).toMatchObject({
+        id: expect.any(String),
+        title: expect.any(String),
+        year: expect.any(Number),
+      });
+      expectNullableAny(movie.title);
+    });
+  });
+
+  it("should add a new movie", async () => {
+    createdMovieID = await movieService.addMovie(newMovie);
+    expect(createdMovieID).toBeTruthy();
+  });
+});
